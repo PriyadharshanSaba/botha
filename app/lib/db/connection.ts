77a06614574da467a39f21421a59469e46f2f5da
PriyadharshanSaba <priyadharshan.97@gmail.com>
2026-05-27 14:32:14 +0530

    updated db url

--- app/lib/db/connection.ts
+++ app/lib/db/connection.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = process.env.DB_URL!;
 
 const pool = new Pool({
   connectionString,
